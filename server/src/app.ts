d26fd63aba79164735e467351889a6065a3ea251
Nattalic <nordonez376@gmail.com>
2026-04-13 11:42:29 -0500

    chore: scaffold initial product module structure

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,29 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-import { PORT, NODE_ENV } from './config/index.js';
+import { env } from './config/index.js';
 import { authRouter } from './features/auth/auth.router.js';
 import { errorsMiddleware } from './middlewares/errorsMiddleware.js';
 
-//Express Configuration
+// Express Configuration
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-//Main Route
+// Main Route
 app.get('/', (req, res) => {
   res.send('The PickU Server is up and running');
 });
 
-//Features Routes
+// Features Routes
 app.use('/picku/api/auth', authRouter);
 
-//Error middleware
+// Error middleware
 app.use(errorsMiddleware);
 
-//App Env
-if (NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// App Env
+if (env.NODE_ENV !== 'production') {
+  app.listen(env.PORT, () => {
+    console.log(`Server is running on http://localhost:${env.PORT}`);
   });
 }
 
