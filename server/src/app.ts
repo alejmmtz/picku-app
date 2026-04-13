40ba55d2708536209959219fb94cb6d9f0e5a84d
Nattalic <nordonez376@gmail.com>
2026-04-13 10:57:52 -0500

    feat: implement entrepreneur module endpoints and business logic

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import { PORT, NODE_ENV } from './config/index.js';
+import { env } from './config/index.js';
 import { authRouter } from './features/auth/auth.router.js';
+import { entrepreneurRouter } from "./features/entrepreneurs/ent.router.js";
 import { errorsMiddleware } from './middlewares/errorsMiddleware.js';
 
 //Express Configuration
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 //Main Route
 app.get('/', (req, res) => {
@@ -15,16 +16,16 @@ app.get('/', (req, res) => {
 });
 
 //Features Routes
-app.use('/picku/api/auth', authRouter);
+app.use('/api', authRouter);
+app.use("/api", entrepreneurRouter);
 
 //Error middleware
-app.use(errorsMiddleware);
+app.use('/picku/api/auth', authRouter);
+app.use('/picku/api/entrepreneurs', entrepreneurRouter);
 
 //App Env
-if (NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+if (env.NODE_ENV !== 'production') {
+  app.listen(env.PORT, () => {
+    console.log(`Server is running on http://localhost:${env.PORT}`);
   });
 }
-
-export default app;
